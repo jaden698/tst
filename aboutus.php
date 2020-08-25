08b74da54edd9bc885c60f2b89b3120972a4499a
jaden698 <67662811+jaden698@users.noreply.github.com>
2020-08-25 13:29:03 +0200

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div class="menu"><?php include "menu.php"?></div>
+    <h1>A propos</h1>
+</body>
+</html>
